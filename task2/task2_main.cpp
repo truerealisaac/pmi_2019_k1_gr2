6cde0050e1a12ccbc1c96e7aed1ea4b25b35d994
Vsevolod Dyraev <slag16rus@gmail.com>
2020-03-27 12:22:56 +0300

    task 2 v1.0

---
+++ task2/task2_main.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cstdlib>
+#include<conio.h>
+
+class dinMemory
+{
+private:
+	int *p;
+	int col_el;
+public:
+
+	dinMemory(int a)
+	{
+		p = new int[a];
+		col_el = a;
+
+		for (int i = 0; i < col_el; i++)
+		{
+			p[i] = rand();
+		}
+	}
+
+	dinMemory(const dinMemory & a)
+	{
+		p = a.p;
+		col_el = a.col_el;
+	}
+
+	~dinMemory()
+	{
+		delete[] p;
+		col_el = 0;
+	}
+
+	int* get_p()
+	{
+		return p;
+	}
+	int get_col()
+	{
+		return col_el;
+	}
+};
+
+void printArr(dinMemory &p)
+{
+	int * pTemp = p.get_p();
+	int i = p.get_col();
+
+	for (int j = 0; j < i; j++)
+	{
+
+		std::cout << "p" << j << ": " << pTemp[j] << std::endl;
+	}
+}
+
+
+int main()
+{
+	int col = 4;
+
+	dinMemory m1(col);
+	printArr(m1);
+	_getch();
+	
+	return 0;
+}
