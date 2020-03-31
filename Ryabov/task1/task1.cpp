d25b5e71fea99558d7639e8001e59f31e30110c4
k-rya <61202227+k-rya@users.noreply.github.com>
2020-03-31 13:07:04 +0300

    Add files via upload

---
+++ Ryabov/task1/task1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, sum;
+	printf("first term=");
+	scanf("%d", &a);
+	printf("second term=");
+	scanf("%d", &b);
+	sum = a + b;
+	printf("Summ=%d", sum);
+	return 0;
+}
