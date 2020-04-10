bd9fd6b06f6ad200d9bd6c075d04157d8abfa8a4
k-rya <61202227+k-rya@users.noreply.github.com>
2020-04-10 14:34:02 +0300

    Create drob.h

---
+++ Ryabov/Task3/drob.h
@@ -0,0 +1,20 @@
+class RacDrob {
+public:
+	RacDrob();
+	RacDrob(int _ch, int _zn);
+	void print();
+	void setzn(int _zn);
+	int getzn();
+	void setch(int _ch);
+	int getch();
+	const RacDrob operator+(const RacDrob& drob);
+	const RacDrob operator-(const RacDrob& drob);
+	const RacDrob operator*(const RacDrob& drob);
+	const RacDrob operator/(const RacDrob& drob);
+	friend const   RacDrob& operator++(RacDrob& drob,int);
+private:
+	int zn;
+	int ch;
+	void red();
+
+};
