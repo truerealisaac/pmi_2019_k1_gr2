e6427f949a1bba1f5289ef012c34f212be5a7ea6
user <pinezhanin@yandex.ru>
2020-04-17 17:14:58 +0300

    add task4

---
+++ pinezhanin/task4/rational.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+class rational
+{
+    public:
+
+    rational(const int &p,const int &q);
+
+    rational operator+(const rational& x);
+    rational operator-(const rational& x);
+    rational operator*(const rational& x);
+    rational operator/(const rational& x);
+    rational operator++(int);
+    rational operator--(int);
+    rational& operator++();
+    rational& operator--();
+    friend istream& operator>>(istream& stream, rational& x);
+	friend ostream& operator<<(ostream& stream, const rational& x);
+
+    private:
+    
+    int p, q;
+    int gcd(int a, int b);
+};
