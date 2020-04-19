9b1883e462fcb08d7ac6d745b88b550ce1d91d08
Tikhon Sakharov <andreev.tihon@gmail.com>
2020-04-19 12:54:54 +0300

    Added decision of task4

---
+++ saharov/task4/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "class.hpp"
+
+int main()
+{
+    Drob d1, d2;
+    cin >> d1;
+    cin >> d2;
+    d1 ++;
+    cout << d1;
+    ++ d1;
+    cout << d1;
+
+
+    return 0;
+}
