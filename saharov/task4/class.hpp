b4db1a4d0d1f713521dc1d2ef34db3589ececade
Tikhon Sakharov <andreev.tihon@gmail.com>
2020-04-20 17:08:25 +0300

    include переместил в хэдер

---
+++ saharov/task4/class.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+
+class Drob{
+public:
+    double ch;
+    double zn;
+
+    Drob();
+    Drob(double _ch, double _zn);
+    Drob operator +(const Drob &a);
+    Drob operator -(const Drob &a);
+    Drob operator *(const Drob &a);
+    Drob operator /(const Drob &a);
+    Drob operator ++(int);
+    Drob &operator ++();
+    void Print();
+    friend &operator <<(ostream &out, const Drob &d);
+    friend &operator >> (istream &in, Drob &d);
+};
+
